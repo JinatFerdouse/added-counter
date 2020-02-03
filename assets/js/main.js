fe43f157965a9cb6145fd1a5765bc730ce480be9
JinatFerdouse <58516299+JinatFerdouse@users.noreply.github.com>
2020-02-03 13:22:45 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$('.case-study').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:false,
+	dots:true,
+	autoplay:true,
+	autoplayTimeout:1500,
+	autoplayHoverPause:true,
+	responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.client-say-wrapper').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:1
+        }
+    }
+})
